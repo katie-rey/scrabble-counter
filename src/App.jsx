8dc196db1df5b53b1c81e823cddf652768a20f86
Katie Rey <katierey@Katies-MacBook-Pro.local>
2022-07-13 22:31:37 +1200

    Build scoreboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,16 @@
-import logo from './logo.svg'
+
 import './App.css'
 import 'flowbite';
+import Form from './components/Form';
+import Scoreboard from './components/Scoreboard';
 
 function App() {
   return (
-    <div className="container">
-      <div className="header-container">
-        <div className="title text-3xl font-bold underline">Scrabble Counter</div>
-      </div>
-      <div className="counter-container">
-       
-        
-        <div className="mb-6">
-    <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Player 1</label>
-    <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name"></input>
-
-  </div>
-        <div className="mb-6">
-    <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Player 2</label>
-    <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name"></input>
-
-  </div>
-      </div>
-    </div>
+    <>
+    <Form />
+    <Scoreboard />
+    </>
+   
   )
 }
 
